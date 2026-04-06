f15467a33302c08318ff0f8a5bcb4f670b80b98e
Avraham Lip <lip.avi@gmail.com>
2026-04-06 18:24:34 +0300

    🚀 UX Fixes v2.2.2: Disabled double-tap zoom on maps and removed redundant version label

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'jeruguessr-v2.2.2';
+const assets = [
+  'game.html',
+  'table_data.js?v=2.2.2',
+  'jerusalem_neighborhoods.geojson?v=2.2.2',
+  'jerusalem_bg.png',
+  'icon-192.png',
+  'icon-512.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
